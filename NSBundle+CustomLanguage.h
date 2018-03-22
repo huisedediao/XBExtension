82706fc23010975fccd5d36e048565f1fc2f5b95
huisedediao <401758785@qq.com>
2018-03-22 16:17:40 +0800

    添加设置默认语言分类

---
+++ NSBundle+CustomLanguage.h
@@ -0,0 +1,15 @@
+//
+//  NSBundle+CustomLanguage.h
+//  aw1_plus
+//
+//  Created by he yan on 26/06/2017.
+//  Copyright © 2017 sven. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSBundle (CustomLanguage)
+
++ (void)setLanguage:(NSString*)language;
+
+@end

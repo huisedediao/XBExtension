640d2296647c3512ef5c9f1c51f7c9e995a1063b
huisedediao <401758785@qq.com>
2018-11-21 09:24:24 +0800

    修改符号

--- UITextField+CustomFunctioms.h
+++ UITextField+CustomFunctioms.h
@@ -26,8 +26,8 @@
 
 
 // 修改文本展示区域，一般跟editingRectForBounds一起重写
-- (CGRect)textRectForBounds:(CGRect)bounds；
+- (CGRect)textRectForBounds:(CGRect)bounds;
 
 // 重写来编辑区域，可以改变光标起始位置，以及光标最右到什么地方，placeHolder的位置也会改变
--(CGRect)editingRectForBounds:(CGRect)bounds；
+-(CGRect)editingRectForBounds:(CGRect)bounds;
 @end
